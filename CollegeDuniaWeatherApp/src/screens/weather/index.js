e0487c171f9d1a708f8a169bb1a0a1b3c44d2cfe
sunpreetsinghQuo <sunpreet.singh@quovantis.com>
2020-08-30 03:33:00 +0530

    comment added

--- CollegeDuniaWeatherApp/src/screens/weather/index.js
+++ CollegeDuniaWeatherApp/src/screens/weather/index.js
@@ -4,6 +4,7 @@ import {styles} from './styles';
 import {weatherOperations} from '../../api/weatherOperations';
 import {ForecastView} from '../../components/forecastView';
 import {LoadingView} from '../../components/LoadingView';
+import NetworkContext from '../../utils/networkContext';
 
 class Weather extends Component {
   constructor(props) {
@@ -23,24 +24,33 @@ class Weather extends Component {
     this.setState({loading: true});
     try {
       const {data} = await weatherOperations.getCurrentWeather();
-      console.log(data, ':::data');
       this.setState({loading: false});
+      // Current Weather
       const currentData = data && data.current;
+      // 5 day Forecast weather
       const dailyData = data && data.daily.splice(3, 7);
       if (dailyData && currentData) {
         this.setState({currentData, dailyData});
       }
     } catch (error) {
+      // Error handle
       this.setState({loading: false});
       this.setState({error: true});
     }
   };
 
   renderView() {
+    // handle Network
+    const hasNetwork =
+      NetworkContext &&
+      NetworkContext._currentValue &&
+      NetworkContext._currentValue.isInternetReaachable;
     const {currentData, dailyData, loading, error} = this.state;
+    // Render screen as per loading and error
+    // If network is off it will show something went wrong
     if (loading) {
       return <LoadingView />;
-    } else if (error) {
+    } else if (error || !hasNetwork) {
       return (
         <View style={styles.errorContainer}>
           <Text style={styles.retryTextWrapper}>
@@ -65,6 +75,7 @@ class Weather extends Component {
 
   render() {
     return (
+      // Used SafeAreaView for notch display
       <SafeAreaView style={styles.container}>{this.renderView()}</SafeAreaView>
     );
   }
